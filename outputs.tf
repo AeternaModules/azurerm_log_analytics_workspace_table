bcb6e7d205bba35ab3aad2419c3380547b037996
Daniel Allen <dan@aeternagroup.net>
2026-01-11 15:41:37 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "log_analytics_workspace_tables" {
+  description = "All log_analytics_workspace_table resources"
+  value       = azurerm_log_analytics_workspace_table.log_analytics_workspace_tables
+}
+
