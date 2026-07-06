c9998ea34d99ef046dbecbfc30f8c740618c0a07
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:13:23 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,24 @@ output "log_analytics_workspace_tables" {
   description = "All log_analytics_workspace_table resources"
   value       = azurerm_log_analytics_workspace_table.log_analytics_workspace_tables
 }
+output "log_analytics_workspace_tables_name" {
+  description = "List of name values across all log_analytics_workspace_tables"
+  value       = [for k, v in azurerm_log_analytics_workspace_table.log_analytics_workspace_tables : v.name]
+}
+output "log_analytics_workspace_tables_plan" {
+  description = "List of plan values across all log_analytics_workspace_tables"
+  value       = [for k, v in azurerm_log_analytics_workspace_table.log_analytics_workspace_tables : v.plan]
+}
+output "log_analytics_workspace_tables_retention_in_days" {
+  description = "List of retention_in_days values across all log_analytics_workspace_tables"
+  value       = [for k, v in azurerm_log_analytics_workspace_table.log_analytics_workspace_tables : v.retention_in_days]
+}
+output "log_analytics_workspace_tables_total_retention_in_days" {
+  description = "List of total_retention_in_days values across all log_analytics_workspace_tables"
+  value       = [for k, v in azurerm_log_analytics_workspace_table.log_analytics_workspace_tables : v.total_retention_in_days]
+}
+output "log_analytics_workspace_tables_workspace_id" {
+  description = "List of workspace_id values across all log_analytics_workspace_tables"
+  value       = [for k, v in azurerm_log_analytics_workspace_table.log_analytics_workspace_tables : v.workspace_id]
+}
 
