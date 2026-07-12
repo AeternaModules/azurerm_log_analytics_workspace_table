96d7c16ce4506d6b26a3092047d088ab9b8f8072
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:29 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "log_analytics_workspace_tables" {
-  description = "All log_analytics_workspace_table resources"
-  value       = azurerm_log_analytics_workspace_table.log_analytics_workspace_tables
-}
 output "log_analytics_workspace_tables_name" {
-  description = "List of name values across all log_analytics_workspace_tables"
-  value       = [for k, v in azurerm_log_analytics_workspace_table.log_analytics_workspace_tables : v.name]
+  description = "Map of name values across all log_analytics_workspace_tables, keyed the same as var.log_analytics_workspace_tables"
+  value       = { for k, v in azurerm_log_analytics_workspace_table.log_analytics_workspace_tables : k => v.name }
 }
 output "log_analytics_workspace_tables_plan" {
-  description = "List of plan values across all log_analytics_workspace_tables"
-  value       = [for k, v in azurerm_log_analytics_workspace_table.log_analytics_workspace_tables : v.plan]
+  description = "Map of plan values across all log_analytics_workspace_tables, keyed the same as var.log_analytics_workspace_tables"
+  value       = { for k, v in azurerm_log_analytics_workspace_table.log_analytics_workspace_tables : k => v.plan }
 }
 output "log_analytics_workspace_tables_retention_in_days" {
-  description = "List of retention_in_days values across all log_analytics_workspace_tables"
-  value       = [for k, v in azurerm_log_analytics_workspace_table.log_analytics_workspace_tables : v.retention_in_days]
+  description = "Map of retention_in_days values across all log_analytics_workspace_tables, keyed the same as var.log_analytics_workspace_tables"
+  value       = { for k, v in azurerm_log_analytics_workspace_table.log_analytics_workspace_tables : k => v.retention_in_days }
 }
 output "log_analytics_workspace_tables_total_retention_in_days" {
-  description = "List of total_retention_in_days values across all log_analytics_workspace_tables"
-  value       = [for k, v in azurerm_log_analytics_workspace_table.log_analytics_workspace_tables : v.total_retention_in_days]
+  description = "Map of total_retention_in_days values across all log_analytics_workspace_tables, keyed the same as var.log_analytics_workspace_tables"
+  value       = { for k, v in azurerm_log_analytics_workspace_table.log_analytics_workspace_tables : k => v.total_retention_in_days }
 }
 output "log_analytics_workspace_tables_workspace_id" {
-  description = "List of workspace_id values across all log_analytics_workspace_tables"
-  value       = [for k, v in azurerm_log_analytics_workspace_table.log_analytics_workspace_tables : v.workspace_id]
+  description = "Map of workspace_id values across all log_analytics_workspace_tables, keyed the same as var.log_analytics_workspace_tables"
+  value       = { for k, v in azurerm_log_analytics_workspace_table.log_analytics_workspace_tables : k => v.workspace_id }
 }
 
